ceeb660903da653864781579c98c5de1c350d3ef
Manabu Niseki <manabu.niseki@gmail.com>
2023-01-29 09:27:06 +0900

    refactor: use MODIFIER_REGEX_FLAGS

--- azuma/utils.py
+++ azuma/utils.py
@@ -40,6 +40,7 @@ def process_field_name(field_string: str):
     name_and_modifiers = field_string.split("|")
     name = name_and_modifiers.pop(0)
     modifiers = [_m for _m in name_and_modifiers if _m]
+
     unsupported = set(modifiers) - SUPPORTED_MODIFIERS
     if unsupported:
         raise UnsupportedFeature(f"Unsupported field modifiers used: {unsupported}")
@@ -64,6 +65,7 @@ def sigma_string_to_regex(original_value: str):
     while value:
         # Grab any content up to the first wildcard
         match = UPTO_WILDCARD.match(value)
+
         if match:
             # The non regex content in the sigma string, may have characters special to regex
             matched = match.group(0)
@@ -110,6 +112,9 @@ def get_modified_value(value: str, modifiers: list[str] | None) -> str:
     return value
 
 
+MODIFIER_REGEX_FLAGS = re.IGNORECASE | re.V1 | re.DOTALL
+
+
 def apply_modifiers(value: str, modifiers: list[str]) -> types.Query:
     """
     Apply as many modifiers as we can during signature construction
@@ -118,14 +123,20 @@ def apply_modifiers(value: str, modifiers: list[str]) -> types.Query:
 
     # If there are wildcards, or we are using the regex modifier, compile the query
     # string to a regex pattern object
-    if "re" in modifiers:
-        return re.compile(value, flags=re.IGNORECASE | re.V1 | re.DOTALL)
+    has_re = "re" in modifiers
+    has_multiple_modifiers = len(modifiers) > 1
+
+    if has_re and has_multiple_modifiers:
+        raise ValueError("re modifier cannot use along with other modifiers")
+
+    if has_re:
+        return re.compile(value, flags=MODIFIER_REGEX_FLAGS)
 
     if not ESCAPED_WILDCARD_PATTERN.fullmatch(value):
         # Transform the unescaped wildcards to their regex equivalent
         reg_value = sigma_string_to_regex(value)
         value = get_modified_value(reg_value, modifiers)
-        return re.compile(value, flags=re.IGNORECASE | re.V1 | re.DOTALL)
+        return re.compile(value, flags=MODIFIER_REGEX_FLAGS)
 
     value = get_modified_value(value, modifiers)
     # If we are just doing a full string compare of a raw string, the comparison
