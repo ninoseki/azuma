bef5dae2ed2e0b88c6df36f229200fbe3c221980
Manabu Niseki <manabu.niseki@gmail.com>
2023-01-29 09:26:38 +0900

    test: add test for multi-line matching

---
+++ tests/schemas/rule/test_multi_lines.py
@@ -0,0 +1,33 @@
+import pytest
+
+from azuma import schemas
+
+
+@pytest.fixture
+def rule():
+    return schemas.Rule.parse_raw(
+        """
+title: startswith
+detection:
+  foo:
+    - aa
+    - bb
+  condition: foo
+logsource:
+  category: test
+    """
+    )
+
+
+@pytest.mark.parametrize(
+    "event,expected",
+    [
+        ({"a": "aa\nbb"}, True),
+        ({"a": "foo\naa"}, True),
+        ({"a": "bar\naa"}, True),
+        ({"a": "a\na"}, False),
+        ({"a": "b\nb"}, False),
+    ],
+)
+def test_with_multi_line(event: dict, expected: bool, rule: schemas.Rule):
+    assert rule.match(event) is expected
