7becd7cf7729b41f99072ef8914335a7a6687f74
Manabu Niseki <manabu.niseki@gmail.com>
2024-12-22 10:31:25 +0900

    refactor: refactor complex function (#45)

--- tests/schemas/rule/test_base64offset.py
+++ tests/schemas/rule/test_base64offset.py
@@ -27,6 +27,8 @@ logsource:
     [
         ({"a": base64.encodebytes(b"/bin/bash").decode()}, True),
         ({"a": base64.b64encode(b"/bin/sh").decode()}, True),
+        ({"a": base64.encodebytes(b"/BIN/BASH").decode()}, False),
+        ({"a": base64.b64encode(b"/BIN/SH").decode()}, False),
         ({"a": "L2Jpbi9iYXNo"}, True),
         ({"a": "9iaW4vYmFza"}, True),
         ({"a": "vYmluL2Jhc2"}, True),
